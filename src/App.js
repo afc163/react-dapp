b0c09e1232d0603cc7e86de5ba63273b04460eb7
afc163 <afc163@gmail.com>
2021-04-15 15:49:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ethers } from 'ethers'
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 
 // Update with the contract address logged out to the CLI when it was deployed 
-const greeterAddress = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
   // store greeting in local state
@@ -39,7 +39,6 @@ function App() {
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
       const transaction = await contract.setGreeting(greeting)
       await transaction.wait()
-      console.log(1111);
       fetchGreeting()
     }
   }
